239e18ae6eaa6ac1e9551f3393941863fb23426e
Markshall <markeriksson94@live.co.uk>
2022-02-15 09:23:26 +0000

    add ability to obfuscate only certain files through `filesToObfuscate` option

--- snowpack-javascript-obfuscator.js
+++ snowpack-javascript-obfuscator.js
@@ -1,14 +1,27 @@
 const JavaScriptObfuscator = require("javascript-obfuscator");
+const path = require("path");
 
 module.exports = (snowpackConfig, pluginOptions = {}) => {
   return {
     name: "snowpack-javascript-obfuscator",
 
-    async transform({ contents, fileExt }) {
-      if (fileExt === ".js") {
+    async transform({ contents, fileExt, filePath }) {
+      // check if we are only obfuscating specific files
+      if ("filesToObfuscate" in pluginOptions) {
+        if (
+          fileExt === ".js" &&
+          pluginOptions.filesToObfuscate.includes(filePath)
+        ) {
+          return JavaScriptObfuscator.obfuscate(
+            contents,
+            pluginOptions.jsObfuscatorOpts ?? {}
+          ).getObfuscatedCode();
+        }
+      } else {
+        // if not, obfuscate all
         return JavaScriptObfuscator.obfuscate(
           contents,
-          pluginOptions
+          pluginOptions.jsObfuscatorOpts ?? {}
         ).getObfuscatedCode();
       }
     },
